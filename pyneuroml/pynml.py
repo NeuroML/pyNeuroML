bb6042b5d9cd001cfb833dac9e25a9fa554466cf
Padraig Gleeson <p.gleeson@gmail.com>
2015-02-02 12:27:49 +0000

    More local tests

--- pyneuroml/pynml.py
+++ pyneuroml/pynml.py
@@ -7,9 +7,8 @@ Thanks to Werner van Geit for initiating this
 
 import os
 import subprocess
-import shutil
-from xml.dom import minidom
 
+verbose = False
 
 def parse_arguments():
     """Parse command line arguments"""
@@ -17,16 +16,19 @@ def parse_arguments():
 
     parser = argparse.ArgumentParser(description='Python utilities for NeuroML2')
 
-    parser.add_argument('xml_filename', metavar='xmlfile', type=str,
-                        help='The xml file to process')
+    parser.add_argument('neuroml2_file', metavar='neuroml2_file', type=str,
+                        help='The NeuroML2 file to process')
 
-    parser.add_argument('--sim', choices=('pylems', 'jlems'),
+    parser.add_argument('-sim', choices=('pylems', 'jlems'),
                         help='Simulator to use')
 
-    parser.add_argument('--validate', action='store_true',
+    parser.add_argument('-validate', action='store_true',
                         help='Only validate')
+                        
+    parser.add_argument('-verbose', action='store_true',
+                        help='Verbose output')
 
-    parser.add_argument('--outputdir', nargs=1,
+    parser.add_argument('-outputdir', nargs=1,
                         help='Directory to write output scripts to')
 
     return parser.parse_args()
@@ -34,6 +36,9 @@ def parse_arguments():
 
 def run_jnml(args):
     """Run the jnml command"""
+    
+    global verbose 
+    verbose = args.verbose
 
     if args.outputdir:
         initialize_outputdir(args.outputdir[0], args.xml_filename)
@@ -52,13 +57,14 @@ def run_jnml(args):
 
     jar = os.path.join(script_dir, "lib/jNeuroML-0.7.0-jar-with-dependencies.jar")
     
-    print(script_dir)
+    print_comment(script_dir)
 
-    execute_command_in_dir("java -jar %s %s %s" %
-                          (jar, args.xml_filename,
-                              simulator_option), exec_dir)
-
-
+    output = execute_command_in_dir("java -jar %s %s %s" %
+                                        (jar, args.neuroml2_file,
+                                            simulator_option), exec_dir)
+                                            
+    print_comment(output)
+                              
 
 def main():
     """Main"""
@@ -66,16 +72,25 @@ def main():
     args = parse_arguments()
 
     run_jnml(args)
+    
+def print_comment(text):
+    
+    if verbose:
+        print("pyNeuroML >>> %s"%(text))
 
 
 def execute_command_in_dir(command, directory):
+    
     """Execute a command in specific working directory"""
+    
     if os.name == 'nt':
         directory = os.path.normpath(directory)
-    print ">>>  Executing: (%s) in dir: %s" % (command, directory)
+        
+    print_comment("Executing: (%s) in dir: %s" % (command, directory))
     
     return_string = subprocess.Popen(command, cwd=directory, shell=True,
                                      stdout=subprocess.PIPE).communicate()[0]
+                                     
     return return_string
 
 
