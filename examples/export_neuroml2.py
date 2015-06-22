924ac10438afaf3ffc67ad3e2603a6d25db699b8
Padraig Gleeson <p.gleeson@gmail.com>
2015-06-22 15:55:57 +0100

    Updates to export to nml2

--- examples/export_neuroml2.py
+++ examples/export_neuroml2.py
@@ -1,8 +1,8 @@
 from pyneuroml.neuron import export_to_neuroml2
 
-export_to_neuroml2("test.hoc", "test.morphonly.nml", includeBiophysicalProperties=False)
+export_to_neuroml2("test.hoc", "test.morphonly.cell.nml", includeBiophysicalProperties=False)
 
-#export_to_neuroml2("test.hoc", "test.biophys.nml", includeBiophysicalProperties=True)
+export_to_neuroml2("test.hoc", "test.biophys.cell.nml", includeBiophysicalProperties=True)
 
 
 
