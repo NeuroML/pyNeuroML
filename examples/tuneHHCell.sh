1c543c62c43ff14f8ecf23398381bcc0653dafa4
Padraig Gleeson <p.gleeson@gmail.com>
2016-02-25 14:07:27 +0000

    Updating to latest jnml jar

--- examples/tuneHHCell.sh
+++ examples/tuneHHCell.sh
@@ -1,4 +1,4 @@
 pynml-tune TestHHsh test_data/HHCellNetwork.net.nml HHCellNetwork \
     [cell:hhcell/channelDensity:naChans/mS_per_cm2,cell:hhcell/channelDensity:kChans/mS_per_cm2] \
-    [200,60] [50,10] [hhpop[0]/v:max_peak_no:30] [hhpop[0]/v:max_peak_no:1] \
-    -simTime 700 -populationSize 10 -maxEvaluations 20 -numSelected 5 -numOffspring 5 $1 $2 $3 $4
+    [200,60] [50,10] [hhpop[0]/v:mean_spike_frequency:70] [hhpop[0]/v:mean_spike_frequency:1] \
+    -simTime 700 -populationSize 10 -maxEvaluations 25 -numSelected 5 -numOffspring 5 -simulator jNeuroML_NEURON $1 $2 $3 $4
