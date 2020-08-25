075dc6b86e89d3a032933a55aa8313dc63cb47c2
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-08-25 17:58:16 +0100

    Cosmetic: more flake fixes

--- examples/reload_data.py
+++ examples/reload_data.py
@@ -4,20 +4,19 @@ from pyneuroml import pynml
 
 example_lems_file = 'LEMS_NML2_Ex5_DetCell.xml'
 
-pynml.reload_saved_data(example_lems_file, 
+pynml.reload_saved_data(example_lems_file,
                         plot=True,
                         show_plot_already=False)
-                        
-                        
+
+
 example_lems_file = 'LEMS_NML2_Ex9_FN.xml'
 
-pynml.reload_saved_data(example_lems_file, 
+pynml.reload_saved_data(example_lems_file,
                         plot=True,
                         show_plot_already=False)
-                        
-                        
-if not '-nogui' in sys.argv:
-    
+
+
+if '-nogui' not in sys.argv:
     from matplotlib import pyplot as plt
-    
+
     plt.show()
--- examples/run_jneuroml_plot_matplotlib.py
+++ examples/run_jneuroml_plot_matplotlib.py
@@ -1,5 +1,4 @@
 import sys
-
 from pyneuroml import pynml
 
 
@@ -11,35 +10,29 @@ example_lems_file = 'LEMS_NML2_Ex5_DetCell.xml'
 results1 = pynml.run_lems_with_jneuroml(example_lems_file, nogui=True, load_saved_data=True)
 
 
-
 ####################################################################
 #   Convert LEMS/NeuroML2 file to NEURON with jNeuroML & run
 
 
-if not '-noneuron' in sys.argv:  # To allow skipping of this for ease of testing
-
+if '-noneuron' not in sys.argv:  # To allow skipping of this for ease of testing
     results2 = pynml.run_lems_with_jneuroml_neuron(example_lems_file, nogui=True, load_saved_data=True)
-
 
 
 ####################################################################
 #   Reload & plot results
 
-if not '-nogui' in sys.argv:
-    
+if '-nogui' not in sys.argv:
     from matplotlib import pyplot as plt
 
     for key in results1.keys():
-
         plt.xlabel('Time (ms)')
         plt.ylabel('...')
         plt.grid('on')
 
         if key != 't':
-            plt.plot(results1['t'],results1[key], label="jNeuroML: "+key)
-            if not '-noneuron' in sys.argv:
-                plt.plot(results2['t'],results2[key], label="jNeuroML_NEURON: "+key)
-        plt.legend(loc=2, fontsize = 'x-small')
-
+            plt.plot(results1['t'], results1[key], label="jNeuroML: " + key)
+            if '-noneuron' not in sys.argv:
+                plt.plot(results2['t'], results2[key], label="jNeuroML_NEURON: " + key)
+        plt.legend(loc=2, fontsize='x-small')
 
     plt.show()
