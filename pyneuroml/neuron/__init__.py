6a0f42f6aa7219cad09fde70259617ae55ea9db7
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-07-05 16:53:26 +0100

    refactor: remove print_comment_v function

--- pyneuroml/neuron/__init__.py
+++ pyneuroml/neuron/__init__.py
@@ -9,7 +9,7 @@ Will use some some utilities from https://github.com/OpenSourceBrain/NEURONShowc
 from pyneuroml.pynml import validate_neuroml1
 from pyneuroml.pynml import validate_neuroml2
 
-from pyneuroml.pynml import print_comment_v
+from pyneuroml.pynml import print_comment
 import os
 
 from pyneuroml.neuron.nrn_export_utils import set_erev_for_mechanism
@@ -26,18 +26,18 @@ def export_to_neuroml2(hoc_or_python_file,
 
     if hoc_or_python_file is not None:
         if hoc_or_python_file.endswith(".py"):
-            print_comment_v("***************\nImporting Python scripts not yet implemented...\n***************")
+            print_comment("***************\nImporting Python scripts not yet implemented...\n***************")
         else:
             if not os.path.isfile(hoc_or_python_file):
-                print_comment_v("***************\nProblem importing file %s (%s)..\n***************" % (hoc_or_python_file, os.path.abspath(hoc_or_python_file)))
+                print_comment("***************\nProblem importing file %s (%s)..\n***************" % (hoc_or_python_file, os.path.abspath(hoc_or_python_file)))
             h.load_file(1, hoc_or_python_file)  # Using 1 to force loading of the file, in case file with same name was loaded before...
     else:
-        print_comment_v("hoc_or_python_file variable is None; exporting what's currently in memory...")
+        print_comment("hoc_or_python_file variable is None; exporting what's currently in memory...")
 
     for ion in known_rev_potentials.keys():
         set_erev_for_mechanism(ion, known_rev_potentials[ion])
 
-    print_comment_v("Loaded NEURON file: %s" % hoc_or_python_file)
+    print_comment("Loaded NEURON file: %s" % hoc_or_python_file)
 
     h.load_file("mview.hoc")
 
@@ -61,12 +61,12 @@ def export_to_neuroml2(hoc_or_python_file,
 
 def export_to_neuroml1(hoc_file, nml1_file_name, level=1, validate=True):
     if not (level == 1 or level == 2):
-        print_comment_v("Only options for Levels in NeuroMLv1.8.1 are 1 or 2")
+        print_comment("Only options for Levels in NeuroMLv1.8.1 are 1 or 2")
         return None
 
     h.load_file(hoc_file)
 
-    print_comment_v("Loaded NEURON file: %s" % hoc_file)
+    print_comment("Loaded NEURON file: %s" % hoc_file)
 
     h.load_file("mview.hoc")
 
