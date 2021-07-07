c90b116789bb3f9b7c141044f9d87817fc1cb11f
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-07-07 14:57:26 +0100

    docs(pynml): document run_lems_with_jneuroml_netpyne

--- pyneuroml/pynml.py
+++ pyneuroml/pynml.py
@@ -1161,6 +1161,42 @@ def run_lems_with_jneuroml_netpyne(
     exit_on_fail=True,
     cleanup=False
 ):
+    # type: (str, typing.List[str], str, bool, bool, int, bool, bool, bool, bool, str, bool, bool, bool, bool) -> bool
+    """Run LEMS file with the NEURON simulator
+
+    Tip: set `skip_run=True` to only parse the LEMS file but not run the simulation.
+
+    :param lems_file_name: name of LEMS file to run
+    :type lems_file_name: str
+    :param paths_to_include: additional directory paths to include (for other NML/LEMS files, for example)
+    :type paths_to_include: list(str)
+    :param max_memory: maximum memory allowed for use by the JVM
+    :type max_memory: bool
+    :param skip_run: toggle whether run should be skipped, if skipped, file will only be parsed
+    :type skip_run: bool
+    :param nogui: toggle whether jnml GUI should be shown
+    :type nogui: bool
+    :param num_processors: number of processors to use for running NetPyNE
+    :type num_processors: int
+    :param load_saved_data: toggle whether any saved data should be loaded
+    :type load_saved_data: bool
+    :param reload_events: toggle whether events should be reloaded
+    :type reload_events: bool
+    :param plot: toggle whether specified plots should be plotted
+    :type plot: bool
+    :param show_plot_already: toggle whether prepared plots should be shown
+    :type show_plot_already: bool
+    :param exec_in_dir: working directory to execute LEMS simulation in
+    :type exec_in_dir: str
+    :param only_generate_scripts: toggle whether only the runner script should be generated
+    :type only_generate_scripts: bool
+    :param verbose: toggle whether jnml should print verbose information
+    :type verbose: bool
+    :param exit_on_fail: toggle whether command should exit if jnml fails
+    :type exit_on_fail: bool
+    :param cleanup: toggle whether the directory should be cleaned of generated files after run completion
+    :type cleanup: bool
+    """
 
     print_comment("Loading LEMS file: {} and running with jNeuroML_NetPyNE".format(lems_file_name))
 
