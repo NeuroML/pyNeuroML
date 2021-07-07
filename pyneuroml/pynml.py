6e76ae999fbe732e3f7cc6c5e8b7c1685fd4bc4b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-07-07 14:32:44 +0100

    docs(pynml): document run_lems_with_jneuroml

--- pyneuroml/pynml.py
+++ pyneuroml/pynml.py
@@ -873,7 +873,38 @@ def run_lems_with_jneuroml(lems_file_name,
                            verbose=DEFAULTS['v'],
                            exit_on_fail=True,
                            cleanup=False):
+    # type: (str, typing.List[str], str, bool, bool, bool, bool, bool, bool, str, bool, bool, bool) -> bool
+    """Parse/Run a LEMS file with jnml.
 
+    Tip: set `skip_run=True` to only parse the LEMS file but not run the simulation.
+
+    :param lems_file_name: name of LEMS file to run
+    :type lems_file_name: str
+    :param paths_to_include: additional directory paths to include (for other NML/LEMS files, for example)
+    :type paths_to_include: list(str)
+    :param max_memory: maximum memory allowed for use by the JVM
+    :type max_memory: book
+    :param skip_run: toggle whether run should be skipped, if skipped, file will only be parsed
+    :type skip_run: bool
+    :param nogui: toggle whether jnml GUI should be shown
+    :type nogui: bool
+    :param load_saved_data: toggle whether any saved data should be loaded
+    :type load_saved_data: bool
+    :param reload_events: toggle whether events should be reloaded
+    :type reload_events: bool
+    :param plot: toggle whether specified plots should be plotted
+    :type plot: bool
+    :param show_plot_already: toggle whether prepared plots should be shown
+    :type show_plot_already: bool
+    :param exec_in_dir: working directory to execute LEMS simulation in
+    :type exec_in_dir: str
+    :param verbose: toggle whether jnml should print verbose information
+    :type verbose: bool
+    :param exit_on_fail: toggle whether command should exit if jnml fails
+    :type exit_on_fail: bool
+    :param cleanup: toggle whether the directory should be cleaned of generated files after run completion
+    :type cleanup: bool
+    """
     print_comment("Loading LEMS file: {} and running with jNeuroML".format(lems_file_name))
     post_args = ""
     post_args += gui_string(nogui)
