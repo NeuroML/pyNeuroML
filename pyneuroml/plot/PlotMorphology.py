28cf40ea00c2ac3e0623070d790071d3024cb516
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-07-28 17:32:07 +0100

    chore: format with black

--- pyneuroml/plot/PlotMorphology.py
+++ pyneuroml/plot/PlotMorphology.py
@@ -1,4 +1,3 @@
-
 import argparse
 
 import re
@@ -12,6 +11,7 @@ def convert_case(name):
     """Converts from camelCase to under_score"""
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+
 
 def build_namespace(a=None, **kwargs):
     if a is None:
@@ -34,12 +34,11 @@ def build_namespace(a=None, **kwargs):
     return a
 
 
-
 DEFAULTS = {
     "v": False,
     "nogui": False,
     "saveToFile": None,
-    }
+}
 
 
 def process_args():
@@ -47,9 +46,7 @@ def process_args():
     Parse command-line arguments.
     """
     parser = argparse.ArgumentParser(
-        description=(
-            "A script which can generate plots of morphologies in NeuroML 2"
-        )
+        description=("A script which can generate plots of morphologies in NeuroML 2")
     )
 
     parser.add_argument(
@@ -64,7 +61,10 @@ def process_args():
     )
 
     parser.add_argument(
-        "-nogui", action="store_true", default=DEFAULTS["nogui"], help="Don't open plot window"
+        "-nogui",
+        action="store_true",
+        default=DEFAULTS["nogui"],
+        help="Don't open plot window",
     )
 
     parser.add_argument(
@@ -75,8 +75,8 @@ def process_args():
         help="Name of the image file",
     )
 
-
     return parser.parse_args()
+
 
 def main(args=None):
     if args is None:
@@ -90,37 +90,41 @@ def plot_2D(a=None, **kwargs):
     print(a)
 
     if a.v:
-        print("Plotting %s"%a.nml_file)
+        print("Plotting %s" % a.nml_file)
 
     from pyneuroml.pynml import read_neuroml2_file
+
     nml_model = read_neuroml2_file(a.nml_file)
 
     for cell in nml_model.cells:
 
-        title = "2D plot of %s from %s"%(cell.id, a.nml_file)
+        title = "2D plot of %s from %s" % (cell.id, a.nml_file)
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.axis('equal')
+        ax.axis("equal")
 
         plt.get_current_fig_manager().set_window_title(title)
-        #plt.title(title)
+        # plt.title(title)
 
         for seg in cell.morphology.segments:
 
             p = cell.get_actual_proximal(seg.id)
             d = seg.distal
             if a.v:
-                print('\nSegment %s, id: %s has proximal point: %s, distal: %s'%(seg.name, seg.id, p, d))
+                print(
+                    "\nSegment %s, id: %s has proximal point: %s, distal: %s"
+                    % (seg.name, seg.id, p, d)
+                )
             width = max(p.diameter, d.diameter)
-            plt.plot([p.x, d.x], [p.y,d.y], linewidth=width, color = 'b')
+            plt.plot([p.x, d.x], [p.y, d.y], linewidth=width, color="b")
 
     if a.save_to_file:
 
-            abs_file = os.path.abspath(a.save_to_file)
+        abs_file = os.path.abspath(a.save_to_file)
 
-            print("Saved image to %s of plot: %s" % (abs_file, title))
-            plt.savefig(abs_file, bbox_inches="tight")
+        print("Saved image to %s of plot: %s" % (abs_file, title))
+        plt.savefig(abs_file, bbox_inches="tight")
 
     if not a.nogui:
         plt.show()
