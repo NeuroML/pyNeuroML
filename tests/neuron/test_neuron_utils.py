1ee8859b0b394b4fb3ce1fecb5ac6d10e27d28dd
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-08-19 15:32:54 +0100

    test(neuro-utils): add simple tests for hoc loader

---
+++ tests/neuron/test_neuron_utils.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Test neuron utils.
+
+File: tests/neuron/test_neuron_utils.py
+
+Copyright 2022 NeuroML contributors
+"""
+
+
+import unittest
+import logging
+import tempfile
+
+
+from pyneuroml.neuron import load_hoc_or_python_file
+
+
+logger = logging.getLogger(__name__)
+
+
+class TestNeuronUtils(unittest.TestCase):
+
+    """Test Neuron Utils"""
+
+    def test_hoc_loader(self):
+        """Test hoc loader util function"""
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".hoc") as f:
+            print(
+                """
+                print "Empty test hoc file"
+                """, file=f, flush=True)
+
+            self.assertTrue(load_hoc_or_python_file(f.name))
+
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".hoc") as f:
+            print(
+                """
+                a line that should cause a syntax error
+                """, file=f, flush=True)
+
+            self.assertFalse(load_hoc_or_python_file(f.name))
+
+        # loading python files is not yet implemented
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as f:
+            print(
+                """
+                print("An empty test python file")
+                """, file=f, flush=True)
+
+            self.assertFalse(load_hoc_or_python_file(f.name))
