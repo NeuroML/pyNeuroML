9928b209fd7f9f1e235043f382e163dccd101231
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-12-06 16:01:58 +0000

    feat(archive): add function to create a COMBINE archive

--- pyneuroml/archive/__init__.py
+++ pyneuroml/archive/__init__.py
@@ -8,9 +8,11 @@ Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 """
 
 
+import os
 import typing
 import logging
 import pathlib
+from zipfile import ZipFile
 from neuroml.loaders import read_neuroml2_file
 from pyneuroml.pynml import extract_lems_definition_files
 from lems.model.model import Model
@@ -50,6 +52,7 @@ def get_model_file_list(
     :type filelist: list of strings
     :param rootdir: directory holding the root file
     :type rootdir: str
+    :raises ValueError: if a file that does not have ".xml" or ".nml" as extension is encountered
     """
     logger.info(f"Processing {rootfile}")
 
@@ -104,3 +107,82 @@ def get_model_file_list(
 
     else:
         raise ValueError(f"File must have a .xml or .nml extension. We got: {rootfile}")
+
+
+def create_combine_archive(zipfile_name: str, rootfile: str, rootdir: str =
+                           ".", zipfile_extension=".neux"):
+    """Create a combine archive that includes all files referred to by the
+    provided rootfile.
+
+    For more information, see:
+
+    Bergmann, F.T., Adams, R., Moodie, S. et al. COMBINE archive and OMEX
+    format: one file to share all information to reproduce a modeling project.
+    BMC Bioinformatics 15, 369 (2014).
+    https://doi.org/10.1186/s12859-014-0369-z
+
+    :param zipfile_name: name of zip file without extension
+    :type zipfile_name: str
+    :param rootfile: main root file
+    :type rootfile: str
+    :param rootdir: directory where root file lives
+    :type rootdir: str
+    :param zipfile_extension: extension for zip file, starting with ".".
+    :type zipfile_extension: str
+    :returns: None
+    """
+    filelist = []  # type: typing.List[str]
+    get_model_file_list(rootfile, filelist, rootdir, None)
+
+    create_combine_archive_manifest(rootfile, filelist, rootdir)
+    filelist.append("manifest.xml")
+
+    # change to directory of rootfile
+    thispath = os.getcwd()
+    os.chdir(rootdir)
+
+    with ZipFile(zipfile_name + zipfile_extension, 'w') as archive:
+        for f in filelist:
+            archive.write(f)
+    os.chdir(thispath)
+
+    print(f"{zipfile_name}{zipfile_extension} created in {rootdir}.")
+
+
+def create_combine_archive_manifest(rootfile: str, filelist: typing.List[str],
+                                    rootdir: str = "."):
+    """Create a combine archive manifest file called manifest.xml
+
+    :param rootfile: the root file for this archive; marked as "master"
+    :type rootfile: str
+    :param filelist: list of files to be included in the manifest
+    :type filelist: list of strings
+    :param rootdir: directory where root file lives
+    :type rootdir: str
+    """
+    manifest = rootdir + "/manifest.xml"
+    with open(manifest, 'w') as mf:
+        print('<?xml version="1.0" encoding="utf-8"?>', file=mf)
+        print(
+            """
+            <omexManifest
+            xmlns="http://identifiers.org/combine.specifications/omex-manifest">
+            """, file=mf)
+
+        print(
+            """
+            <content location="."
+                format="http://identifiers.org/combine.specifications/omex"/>
+            """, file=mf)
+
+        for f in filelist:
+            print(
+                f"""
+                <content location="{f}"
+                    format="http://identifiers.org/combine.specifications/neuroml"/>
+                """, file=mf)
+
+        print(
+            """
+            </omexManifest>
+            """, file=mf, flush=True)
