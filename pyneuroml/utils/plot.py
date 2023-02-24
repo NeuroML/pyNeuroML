28ac70529f0c8665d79de533bc88ac09584a220d
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-02-24 12:09:07 +0000

    chore: format with black

--- pyneuroml/utils/plot.py
+++ pyneuroml/utils/plot.py
@@ -336,10 +336,14 @@ def add_line_to_matplotlib_2D_plot(ax, xv, yv, width, color, axis_min_max):
     axis_min_max[1] = max(axis_min_max[1], xv[1])
 
 
-def create_new_vispy_canvas(view_min: typing.List[float], view_max:
-                            typing.List[float], title: str = "", axes_pos:
-                            typing.Optional[typing.List] = None, axes_length: float = 100,
-                            axes_width: int = 2):
+def create_new_vispy_canvas(
+    view_min: typing.List[float],
+    view_max: typing.List[float],
+    title: str = "",
+    axes_pos: typing.Optional[typing.List] = None,
+    axes_length: float = 100,
+    axes_width: int = 2,
+):
     """Create a new vispy scene canvas with a view and optional axes lines
 
     Reference: https://vispy.org/gallery/scene/axes_plot.html
@@ -358,8 +362,9 @@ def create_new_vispy_canvas(view_min: typing.List[float], view_max:
     :type axes_width: float
     :returns: scene, view
     """
-    canvas = scene.SceneCanvas(keys='interactive', show=True,
-                               bgcolor="white", size=(800, 600))
+    canvas = scene.SceneCanvas(
+        keys="interactive", show=True, bgcolor="white", size=(800, 600)
+    )
     grid = canvas.central_widget.add_grid(margin=10)
     grid.spacing = 0
 
@@ -374,25 +379,29 @@ def create_new_vispy_canvas(view_min: typing.List[float], view_max:
     grid.add_widget(console_widget, row=3, col=1, col_span=1)
     console_text = "\tKeys: 0 reset, 5 changes camera"
 
-    yaxis = scene.AxisWidget(orientation='left',
-                             axis_label='Extent (Y)',
-                             axis_font_size=12,
-                             axis_label_margin=60,
-                             axis_color='black',
-                             tick_color='black',
-                             tick_label_margin=5,
-                             text_color='black')
+    yaxis = scene.AxisWidget(
+        orientation="left",
+        axis_label="Extent (Y)",
+        axis_font_size=12,
+        axis_label_margin=60,
+        axis_color="black",
+        tick_color="black",
+        tick_label_margin=5,
+        text_color="black",
+    )
     yaxis.width_max = 80
     grid.add_widget(yaxis, row=1, col=0)
 
-    xaxis = scene.AxisWidget(orientation='bottom',
-                             axis_label='Extent (X)',
-                             axis_font_size=12,
-                             axis_label_margin=40,
-                             axis_color='black',
-                             tick_color='black',
-                             text_color='black',
-                             tick_label_margin=5)
+    xaxis = scene.AxisWidget(
+        orientation="bottom",
+        axis_label="Extent (X)",
+        axis_font_size=12,
+        axis_label_margin=40,
+        axis_color="black",
+        tick_color="black",
+        text_color="black",
+        tick_label_margin=5,
+    )
 
     xaxis.height_max = 60
     grid.add_widget(xaxis, row=2, col=1)
@@ -403,17 +412,16 @@ def create_new_vispy_canvas(view_min: typing.List[float], view_max:
     bottom_padding = grid.add_widget(row=4, col=0, col_span=3)
     bottom_padding.height_max = 40
 
-    view = grid.add_view(row=1, col=1, border_color='black')
+    view = grid.add_view(row=1, col=1, border_color="black")
     # create cameras
     # https://vispy.org/gallery/scene/flipped_axis.html
-    cam1 = scene.cameras.PanZoomCamera(parent=view.scene, aspect=1,
-                                       name='PanZoom')
+    cam1 = scene.cameras.PanZoomCamera(parent=view.scene, aspect=1, name="PanZoom")
     cam1.center = [view_center[0], view_center[1]]
-    cam2 = scene.cameras.TurntableCamera(parent=view.scene, name='Turntable')
+    cam2 = scene.cameras.TurntableCamera(parent=view.scene, name="Turntable")
     cam2.center = view_center
-    cam3 = scene.cameras.ArcballCamera(parent=view.scene, name='Arcball')
+    cam3 = scene.cameras.ArcballCamera(parent=view.scene, name="Arcball")
     cam3.center = view_center
-    cam4 = scene.cameras.FlyCamera(parent=view.scene, name='Fly')
+    cam4 = scene.cameras.FlyCamera(parent=view.scene, name="Fly")
     cam4.center = view_center
     cam4.autoroll = False
 
@@ -441,14 +449,14 @@ def create_new_vispy_canvas(view_min: typing.List[float], view_max:
         cam4: """
         Arrow keys/WASD to move forward/backwards/left/right; F/C to
         move up and down; Space to brake; Left mouse button/I/K/J/L to
-        control pitch and yaw; Q/E for rolling"""
+        control pitch and yaw; Q/E for rolling""",
     }
 
     for acam in cams.values():
         acam.set_range(
             x=(view_min[0], view_max[0]),
             y=(view_min[1], view_max[1]),
-            z=(view_min[2], view_max[2])
+            z=(view_min[2], view_max[2]),
         )
 
     # Fly is default
@@ -461,50 +469,59 @@ def create_new_vispy_canvas(view_min: typing.List[float], view_max:
 
     @canvas.events.key_press.connect
     def vispy_on_key_press(event):
-        if event.text == '5':
+        if event.text == "5":
             state = view.camera.get_state()
             view.camera = cams[view.camera]
             console_widget.clear()
             console_widget.write(console_text + f"({view.camera.name})")
             try:
-                console_widget.write(textwrap.dedent(cam_text[view.camera]).replace("\n", " "))
+                console_widget.write(
+                    textwrap.dedent(cam_text[view.camera]).replace("\n", " ")
+                )
             except KeyError:
                 pass
             # PanZoom doesn't like it
             if view.camera.name != "PanZoom":
                 try:
-                    view.camera.center = state['center']
-                    view.camera.scale_factor = state['scale_factor']
-                    view.camera.fov = state['fov']
+                    view.camera.center = state["center"]
+                    view.camera.scale_factor = state["scale_factor"]
+                    view.camera.fov = state["fov"]
                 except KeyError:
                     pass
             else:
                 view.camera.set_range(
                     x=(view_min[0], view_max[0]),
                     y=(view_min[1], view_max[1]),
-                    z=(view_min[2], view_max[2])
+                    z=(view_min[2], view_max[2]),
                 )
-        elif event.text == '0':
+        elif event.text == "0":
             for acam in cams.values():
                 acam.set_range(
                     x=(view_min[0], view_max[0]),
                     y=(view_min[1], view_max[1]),
-                    z=(view_min[2], view_max[2])
+                    z=(view_min[2], view_max[2]),
                 )
 
     # xyz axis for orientation
     # TODO improve placement
     if axes_pos:
-        scene.Line([axes_pos, [axes_pos[0] + axes_length, axes_pos[1], axes_pos[2]]],
-                   parent=view.scene, color="black",
-                   width=axes_width)
-        scene.Line([axes_pos, [axes_pos[1], axes_pos[1] + axes_length, axes_pos[2]]],
-                   parent=view.scene, color="black",
-                   width=axes_width)
-        scene.Line([axes_pos, [axes_pos[1], axes_pos[1], axes_pos[2] + axes_length]],
-                   parent=view.scene, color="black",
-                   width=axes_width)
+        scene.Line(
+            [axes_pos, [axes_pos[0] + axes_length, axes_pos[1], axes_pos[2]]],
+            parent=view.scene,
+            color="black",
+            width=axes_width,
+        )
+        scene.Line(
+            [axes_pos, [axes_pos[1], axes_pos[1] + axes_length, axes_pos[2]]],
+            parent=view.scene,
+            color="black",
+            width=axes_width,
+        )
+        scene.Line(
+            [axes_pos, [axes_pos[1], axes_pos[1], axes_pos[2] + axes_length]],
+            parent=view.scene,
+            color="black",
+            width=axes_width,
+        )
 
     return scene, view
-
-
