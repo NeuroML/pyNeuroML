77c6ec483f1b40db4dfe43375b094497f24403af
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-04-19 16:50:24 +0100

    test(cell-rotate): add unit test

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -13,7 +13,7 @@ import pathlib as pl
 import math
 
 import neuroml
-from pyneuroml.pynml import read_neuroml2_file
+from pyneuroml.pynml import read_neuroml2_file, write_neuroml2_file
 from pyneuroml.utils import extract_position_info, rotate_cell
 
 from .. import BaseTestCase
@@ -52,11 +52,42 @@ class TestUtils(BaseTestCase):
     def test_rotate_cell(self):
         """Test rotate_cell"""
         acell = neuroml.utils.component_factory("Cell", id="test_cell", validate=False)  # type: neuroml.Cell
-        acell.add_segment(prox=[0, 0, 0, 5], dist=[1, 1, 1, 5], seg_id=0,
+        soma = acell.add_segment(prox=[0, 0, 0, 1], dist=[0, 0, 0, 1], seg_id=0,
+                                 use_convention=False, reorder_segment_groups=False,
+                                 optimise_segment_groups=False)
+        acell.add_segment(prox=[0, 0, 0, 1], dist=[5, 0, 0, 1], seg_id=1,
                           use_convention=False, reorder_segment_groups=False,
-                          optimise_segment_groups=False)
+                          optimise_segment_groups=False, parent=soma)
+
+        acell.add_segment(prox=[0, 0, 0, 10], dist=[0, 10, 0, 10], seg_id=2,
+                          use_convention=False, reorder_segment_groups=False,
+                          optimise_segment_groups=False, parent=soma)
+
+        acell.add_segment(prox=[0, 0, 0, 25], dist=[0, 0, 25, 25], seg_id=3,
+                          use_convention=False, reorder_segment_groups=False,
+                          optimise_segment_groups=False, parent=soma)
 
         print(acell)
 
-        rotated_cell = rotate_cell(acell, x=math.pi / 4, y=0, z=0, order="xyz")
+        rotated_cell = rotate_cell(acell, x=math.pi / 2, y=0, z=0, order="xyz")
+        rotated_cell.id = "test_rotated_cell"
         print(rotated_cell)
+
+        newdoc = neuroml.utils.component_factory("NeuroMLDocument",
+                                                 id="test_doc")  # type: neuroml.NeuroMLDocument
+        newdoc.add(acell)
+        newdoc.add(rotated_cell)
+
+        net = newdoc.add("Network", id="test_net", validate=False)
+        pop1 = net.add("Population", id="test_pop1", size=1, component=acell.id,
+                       type="populationList", validate=False)
+        pop1.add("Instance", id=0, location=pop1.component_factory("Location", x=0, y=0, z=0))
+
+        pop2 = net.add("Population", id="test_pop2", size=1,
+                       component=rotated_cell.id,
+                       type="populationList", validate=False)
+        pop2.add("Instance", id=0, location=pop1.component_factory("Location",
+                                                                   x=50, y=0, z=0))
+
+        newdoc.validate(recursive=True)
+        write_neuroml2_file(newdoc, "test_rotation.net.nml", validate=True)
