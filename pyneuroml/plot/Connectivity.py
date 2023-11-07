f8c4fe4ec87bdc289f06e55d0808a5a0c72b74a6
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-11-07 17:03:52 +0000

    wip: chord-diagram implementation using mne
    https://mne.tools/mne-connectivity/stable/auto_examples/mne_inverse_label_connectivity.html#make-a-connectivity-plot

--- pyneuroml/plot/Connectivity.py
+++ pyneuroml/plot/Connectivity.py
@@ -33,3 +33,12 @@ def plot_connectivity_matrix(filename, level):
     handler.finalise_document()
 
     logger.info("Done with MatrixHandler...")
+
+
+def plot_chord_diagram(filename):
+    """Plot a chord connectivity diagram
+
+    :param filename: name of NeuroML file
+    :type filename: str
+    """
+    pass
