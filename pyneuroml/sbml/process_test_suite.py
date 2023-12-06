2d1ac9295ff25978c9d4437e792483619f983aeb
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2023-12-06 12:09:44 +0000

    added SEDML validation check to the SBML test suite script

--- pyneuroml/sbml/process_test_suite.py
+++ pyneuroml/sbml/process_test_suite.py
@@ -2,11 +2,16 @@
 
 """
 produce a markdown table of the results of running various tests on the SBML Test Suite
+
+get the version of the test suite that includes sedml versions
+https://github.com/sbmlteam/sbml-test-suite/releases/download/3.4.0/semantic_tests_with_sedml_and_graphs.v3.4.0.zip
+
 """
 
 import os
 import glob
 from pyneuroml.sbml import validate_sbml_files
+from pyneuroml.sedml import validate_sedml_files
 
 
 def parse_arguments():
@@ -48,18 +53,21 @@ def parse_arguments():
 def process_cases(args):
     "process the test cases and write results out as a markdown table"
 
-    header = "|case|valid|strict-units valid|\n"
-    row = "|{case}|{valid}|{strict}|\n"
+    header = "|case|valid-sbml|valid-sbml-units|valid-sedml|\n"
+    row = "|{case}|{valid_sbml}|{valid_sbml_units}|{valid_sedml}|\n"
 
     with open(args.output_file, "w") as fout:
         os.chdir(args.case_path)
         fout.write(header)
         for fpath in glob.glob(args.case_glob):
+            sedml_path = fpath.replace(".xml", "-sedml.xml")
             print(fpath)
             assert os.path.isfile(fpath)
+            assert os.path.isfile(sedml_path)
             case = os.path.basename(fpath)
-            valid = validate_sbml_files([fpath], strict_units=False)
-            strict = validate_sbml_files([fpath], strict_units=True)
+            valid_sbml = validate_sbml_files([fpath], strict_units=False)
+            valid_sbml_units = validate_sbml_files([fpath], strict_units=True)
+            valid_sedml = validate_sedml_files([sedml_path])
             fout.write(row.format(**locals()))
 
 
