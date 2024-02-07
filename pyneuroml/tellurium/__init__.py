a461f6a6832bb8dab48baa4b8b0910cd2381a04d
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-02-07 10:33:19 +0000

    added sedml file validation

--- pyneuroml/tellurium/__init__.py
+++ pyneuroml/tellurium/__init__.py
@@ -2,11 +2,13 @@
 run a model using the tellurium engine
 """
 
+from pyneuroml.sedml import validate_sedml_files
 import tellurium as te
 
 te.setDefaultPlottingEngine("matplotlib")
 
 import sys
+import os
 
 # For technical reasons, any software which uses libSEDML
 # must provide a custom build - Tellurium uses tesedml
@@ -23,7 +25,9 @@ def run_from_sedml_file(input_files):
         raise ValueError("No input files specified")
 
     for file_name in input_files:
-        # check validity of sedml file here
+        # tellurium seems not to do much validation so we do our own here
+        if not validate_sedml_files([file_name]):
+            raise IOError(f"failed to validate SEDML file {file_name}")
 
         try:
             doc = libsedml.readSedML(file_name)
@@ -31,4 +35,4 @@ def run_from_sedml_file(input_files):
             raise IOError(f"readSedML failed trying to open the file {file_name}")
 
         # execute SED-ML using Tellurium
-        te.executeSEDML(doc.toSed(), workingDir=".")
+        te.executeSEDML(doc.toSed(), workingDir=os.path.dirname(file_name))
