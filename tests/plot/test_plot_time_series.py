5b3201466658fe293d18bca6974510fe67cb451a
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-02-22 17:09:56 +0000

    test: add for `test_plot_time_series_from_data_file`

--- tests/plot/test_plot_time_series.py
+++ tests/plot/test_plot_time_series.py
@@ -12,7 +12,9 @@ import os
 import unittest
 
 import numpy
-from pyneuroml.plot.PlotTimeSeries import *
+import logging
+import pyneuroml.plot.PlotTimeSeries as pyplts
+import tempfile
 
 from .. import BaseTestCase
 
@@ -37,14 +39,14 @@ class TestPlotTimeSeries(BaseTestCase):
                 numpy.random.default_rng().uniform(-30, 80, npoints)
             )
 
-        plot_time_series(
+        pyplts.plot_time_series(
             traces_dict,
             title="",
             offset=False,
             show_plot_already=False,
             save_figure_to="time-series-test.png",
         )
-        plot_time_series(
+        pyplts.plot_time_series(
             traces_dict,
             title="",
             offset=True,
@@ -56,6 +58,39 @@ class TestPlotTimeSeries(BaseTestCase):
 
         os.unlink("time-series-test.png")
         os.unlink("time-series-test-2.png")
+
+    def test_plot_time_series_from_data_file(self):
+        """Test plot_time_series_from_data_file function"""
+        trace_file = tempfile.NamedTemporaryFile(mode="w", delete=False, dir=".")
+        for i in range(0, 1000):
+            print(
+                f"{i/1000}\t{numpy.random.default_rng().random()}\t{numpy.random.default_rng().random()}\t{numpy.random.default_rng().random()}",
+                file=trace_file,
+            )
+        trace_file.flush()
+        trace_file.close()
+
+        pyplts.plot_time_series_from_data_files(
+            trace_file.name,
+            title="",
+            offset=False,
+            show_plot_already=False,
+            save_figure_to="time-series-test-from-file.png",
+        )
+        self.assertIsFile("time-series-test-from-file.png")
+
+        pyplts.plot_time_series_from_data_files(
+            trace_file.name,
+            title="",
+            offset=True,
+            show_plot_already=False,
+            save_figure_to="time-series-test-from-file-2.png",
+        )
+        self.assertIsFile("time-series-test-from-file.png")
+
+        os.unlink("time-series-test-from-file.png")
+        os.unlink("time-series-test-from-file-2.png")
+        os.unlink(trace_file.name)
 
 
 if __name__ == "__main__":
