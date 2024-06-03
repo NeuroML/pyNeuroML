f7f099ac82480ca288acb36da62d814f42c1da5b
Aditya Pandey <adityapand3y666@gmail.com>
2024-06-04 02:38:38 +0530

    added node class

--- pyneuroml/swc/SWCLoader.py
+++ pyneuroml/swc/SWCLoader.py
@@ -4,3 +4,17 @@ class Point:
         self.y = float(y)
         self.z = float(z)
         self.diameter = float(diameter)
+
+
+class Node:
+    def __init__(self, node_id, type_id):
+        self.id = int(node_id)
+        self.type_id = int(type_id)
+        self.type = None
+        self.point = None
+        self.parent_id = None
+        self.parent = None
+        self.children = []
+
+    def set_point(self, x, y, z, diameter):
+        self.point = Point(x, y, z, diameter)
