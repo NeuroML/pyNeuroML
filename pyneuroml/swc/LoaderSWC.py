8ac50f85f9dd804593354164e81c8d48addd63be
Aditya Pandey <adityapand3y666@gmail.com>
2024-06-04 11:19:31 +0530

    added SegmentClass

--- pyneuroml/swc/LoaderSWC.py
+++ pyneuroml/swc/LoaderSWC.py
@@ -21,3 +21,14 @@ class Segment:
 
     def set_distal(self, x, y, z, diameter):
         self.distal = Point(x, y, z, diameter)
+
+
+class SegmentGroup:
+    def __init__(self, group_id, name, neurolex_id=None):
+        self.id = group_id
+        self.name = name
+        self.neurolex_id = neurolex_id
+        self.members = set()
+
+    def add_member(self, segment_id):
+        self.members.add(segment_id)
