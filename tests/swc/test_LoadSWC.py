e9a67427bd1c2c04d31761617dcbcc29e94c558b
Aditya Pandey <adityapand3y666@gmail.com>
2024-06-10 15:05:35 +0530

    added TestSWCGraph class

--- tests/swc/test_LoadSWC.py
+++ tests/swc/test_LoadSWC.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyneuroml.swc.LoadSWC import SWCNode
+from pyneuroml.swc.LoadSWC import SWCGraph, SWCNode
 
 
 class TestSWCNode(unittest.TestCase):
@@ -17,3 +17,14 @@ class TestSWCNode(unittest.TestCase):
     def test_invalid_init(self):
         with self.assertRaises(ValueError):
             SWCNode("a", 1, 0.0, 0.0, 0.0, 1.0, -1)
+
+
+class TestSWCGraph(unittest.TestCase):
+    def setUp(self):
+        self.tree = SWCGraph()
+        self.node1 = SWCNode(1, 1, 0.0, 0.0, 0.0, 1.0, -1)
+        self.node2 = SWCNode(2, 3, 1.0, 0.0, 0.0, 0.5, 1)
+        self.node3 = SWCNode(3, 3, 2.0, 0.0, 0.0, 0.5, 2)
+        self.tree.add_node(self.node1)
+        self.tree.add_node(self.node2)
+        self.tree.add_node(self.node3)
