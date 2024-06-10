b4d0b698967b08d8459868f2dd1651300a14ad06
Aditya Pandey <adityapand3y666@gmail.com>
2024-06-10 21:32:28 +0530

    added test_get_graph function

--- tests/swc/test_LoadSWC.py
+++ tests/swc/test_LoadSWC.py
@@ -1,5 +1,7 @@
 import unittest
 
+import networkx as nx
+
 from pyneuroml.swc.LoadSWC import SWCGraph, SWCNode
 
 
@@ -24,7 +26,36 @@ class TestSWCGraph(unittest.TestCase):
         self.tree = SWCGraph()
         self.node1 = SWCNode(1, 1, 0.0, 0.0, 0.0, 1.0, -1)
         self.node2 = SWCNode(2, 3, 1.0, 0.0, 0.0, 0.5, 1)
+        self.node2.fraction_along = 1.0
         self.node3 = SWCNode(3, 3, 2.0, 0.0, 0.0, 0.5, 2)
+        self.node3.fraction_along = 0.5
         self.tree.add_node(self.node1)
         self.tree.add_node(self.node2)
         self.tree.add_node(self.node3)
+
+    def test_duplicate_node(self):
+        with self.assertRaises(ValueError):
+            self.tree.add_node(SWCNode(1, 1, 0.0, 0.0, 0.0, 1.0, -1))
+
+    def test_add_metadata(self):
+        self.tree.add_metadata("ORIGINAL_SOURCE", "file.swc")
+        self.assertEqual(self.tree.metadata["ORIGINAL_SOURCE"], "file.swc")
+
+    def test_invalid_metadata(self):
+        self.tree.add_metadata("INVALID_FIELD", "value")
+        self.assertEqual(self.tree.metadata, {})
+
+    def test_get_segment_adjacency_list(self):
+        adjacency_list = self.tree.get_segment_adjacency_list()
+        self.assertEqual(adjacency_list, {1: [self.node2], 2: [self.node3]})
+
+    def test_get_graph(self):
+        graph = self.tree.get_graph()
+        self.assertIsInstance(graph, nx.DiGraph)
+        self.assertEqual(len(graph.nodes), 3)
+        self.assertEqual(len(graph.edges), 2)
+        self.assertEqual(graph.nodes[self.node1.id]["type"], self.node1.type)
+        self.assertEqual(graph.nodes[self.node2.id]["x"], self.node2.x)
+        self.assertEqual(graph.nodes[self.node3.id]["radius"], self.node3.radius)
+        self.assertEqual(graph.edges[(self.node1.id, self.node2.id)]["weight"], 0.0)
+        self.assertEqual(graph.edges[(self.node2.id, self.node3.id)]["weight"], 0.5)
