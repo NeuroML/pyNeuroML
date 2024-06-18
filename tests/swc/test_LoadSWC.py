ab9a8eb04937749c86b08e1c349afef7eedceea4
Aditya Pandey <adityapand3y666@gmail.com>
2024-06-18 16:20:46 +0530

    replaced the test file for updated LoadSWC.py func

--- tests/swc/test_LoadSWC.py
+++ tests/swc/test_LoadSWC.py
@@ -1,8 +1,10 @@
 import unittest
+from io import StringIO
+from unittest.mock import patch
 
 import networkx as nx
 
-from pyneuroml.swc.LoadSWC import SWCGraph, SWCNode
+from pyneuroml.swc.LoadSWC import SWCGraph, SWCNode, load_swc
 
 
 class TestSWCNode(unittest.TestCase):
@@ -45,21 +47,6 @@ class TestSWCGraph(unittest.TestCase):
         self.tree.add_metadata("INVALID_FIELD", "value")
         self.assertEqual(self.tree.metadata, {})
 
-    def test_get_segment_adjacency_list(self):
-        adjacency_list = self.tree.get_segment_adjacency_list()
-        self.assertEqual(adjacency_list, {1: [self.node2], 2: [self.node3]})
-
-    def test_get_graph(self):
-        graph = self.tree.get_graph()
-        self.assertIsInstance(graph, nx.DiGraph)
-        self.assertEqual(len(graph.nodes), 3)
-        self.assertEqual(len(graph.edges), 2)
-        self.assertEqual(graph.nodes[self.node1.id]["type"], self.node1.type)
-        self.assertEqual(graph.nodes[self.node2.id]["x"], self.node2.x)
-        self.assertEqual(graph.nodes[self.node3.id]["radius"], self.node3.radius)
-        self.assertEqual(graph.edges[(self.node1.id, self.node2.id)]["weight"], 0.0)
-        self.assertEqual(graph.edges[(self.node2.id, self.node3.id)]["weight"], 0.5)
-
     def test_get_parent(self):
         self.assertIsNone(self.tree.get_parent(self.node1.id))
         self.assertEqual(self.tree.get_parent(self.node2.id), self.node1)
@@ -67,33 +54,8 @@ class TestSWCGraph(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.tree.get_parent(4)
 
-    def test_get_descendants(self):
-        self.assertEqual(
-            self.tree.get_descendants(self.node1.id), [self.node2, self.node3]
-        )
-        self.assertEqual(self.tree.get_descendants(self.node2.id), [self.node3])
-        self.assertEqual(self.tree.get_descendants(self.node3.id), [])
+    def test_get_children(self):
+        self.assertEqual(self.tree.get_children(self.node1.id), [self.node2])
+        self.assertEqual(self.tree.get_children(self.node2.id), [self.node3])
         with self.assertRaises(ValueError):
-            self.tree.get_descendants(4)
-
-    def test_get_nodes_with_multiple_children(self):
-        self.assertEqual(self.tree.get_nodes_with_multiple_children(), [])
-        self.assertEqual(self.tree.get_nodes_with_multiple_children(SWCNode.SOMA), [])
-        self.assertEqual(
-            self.tree.get_nodes_with_multiple_children(SWCNode.BASAL_DENDRITE), []
-        )
-
-    def test_get_nodes_by_type(self):
-        self.assertEqual(len(self.tree.get_nodes_by_type(SWCNode.SOMA)), 1)
-        self.assertEqual(len(self.tree.get_nodes_by_type(SWCNode.BASAL_DENDRITE)), 2)
-        self.assertEqual(len(self.tree.get_nodes_by_type(SWCNode.AXON)), 0)
-
-    def test_get_branch_points(self):
-        self.assertEqual(self.tree.get_branch_points(SWCNode.SOMA), [])
-        self.assertEqual(self.tree.get_branch_points(SWCNode.BASAL_DENDRITE), [])
-        self.assertEqual(
-            self.tree.get_branch_points(SWCNode.SOMA, SWCNode.BASAL_DENDRITE), []
-        )
-
-    def test_has_soma_node(self):
-        self.assertTrue(self.tree.has_soma_node())
+            self.tree.get_parent(4)
