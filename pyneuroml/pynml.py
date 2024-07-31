2c35fa74f156cd3f7a8d6e8c859ffee4dfa928aa
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-07-31 15:45:20 +0100

    feat: move moose util to separate submodule

--- pyneuroml/pynml.py
+++ pyneuroml/pynml.py
@@ -33,6 +33,7 @@ from pyneuroml.swc.ExportSWC import convert_to_swc
 from pyneuroml.utils import extract_lems_definition_files
 from pyneuroml.utils.info import *
 from pyneuroml.utils.misc import *
+from pyneuroml.utils.moose import *
 
 # these imports are included for backwards compatibility
 from pyneuroml.utils.units import *
@@ -912,51 +913,6 @@ def reload_standard_dat_file(file_name: str) -> typing.Tuple[dict, list]:
 
         logger.info("Loaded data from %s; columns: %s" % (file_name, indices))
     return data, indices
-
-
-def evaluate_component(comp_type, req_variables={}, parameter_values={}):
-    """
-    Work in progress: expand a (simple) ComponentType  and evaluate an instance of it by
-    giving parameters & required variables
-    Used in MOOSE NeuroML reader...
-    """
-    logger.debug(
-        "Evaluating %s with req:%s; params:%s"
-        % (comp_type.name, req_variables, parameter_values)
-    )
-    exec_str = ""
-    return_vals = {}
-    for p in parameter_values:
-        exec_str += "%s = %s\n" % (p, get_value_in_si(parameter_values[p]))
-    for r in req_variables:
-        exec_str += "%s = %s\n" % (r, get_value_in_si(req_variables[r]))
-    for c in comp_type.Constant:
-        exec_str += "%s = %s\n" % (c.name, get_value_in_si(c.value))
-    for d in comp_type.Dynamics:
-        for dv in d.DerivedVariable:
-            exec_str += "%s = %s\n" % (dv.name, dv.value)
-            exec_str += 'return_vals["%s"] = %s\n' % (dv.name, dv.name)
-        for cdv in d.ConditionalDerivedVariable:
-            for case in cdv.Case:
-                if case.condition:
-                    cond = (
-                        case.condition.replace(".neq.", "!=")
-                        .replace(".eq.", "==")
-                        .replace(".gt.", ">")
-                        .replace(".lt.", "<")
-                    )
-                    exec_str += "if ( %s ): %s = %s \n" % (cond, cdv.name, case.value)
-                else:
-                    exec_str += "else: %s = %s \n" % (cdv.name, case.value)
-
-            exec_str += "\n"
-
-            exec_str += 'return_vals["%s"] = %s\n' % (cdv.name, cdv.name)
-    exec_str = "from math import exp  # only one required for nml2?\n" + exec_str
-    # logger.info('Exec %s'%exec_str)
-    exec(exec_str)
-
-    return return_vals
 
 
 def main(args=None):
