1040db612051d41c3ff2d867e5a4a7c51d0ad2ce
Padraig Gleeson <p.gleeson@gmail.com>
2025-01-21 18:26:13 +0000

    More tests on execute_command_in_dir

--- examples/test_execute_command.py
+++ examples/test_execute_command.py
@@ -1,13 +1,17 @@
 from pyneuroml import pynml
 
-command = 'pwd'
-command = 'ls -alt test'
 run_dir = '.'
 
-returncode, output = pynml.execute_command_in_dir(command, run_dir, prefix="Command [%s]> "%command, verbose=True) 
+commands = ['pwd', 'ls -alt testss']
 
-print('  ----  Return code from execute_command_in_dir: %s; output: \n%s\n--------------------------'%(returncode, output))
+for command in commands:
 
-success = pynml.execute_command_in_dir_with_realtime_output(command, run_dir, prefix="Command [%s]: "%command, verbose=False) 
+    print("\n====================\n")
 
-print('  ----  Success of execute_command_in_dir_with_realtime_output: %s'%success)
+    returncode, output = pynml.execute_command_in_dir(command, run_dir, prefix="Output [%s] > "%command, verbose=True) 
+
+    print('  ----  Return code from execute_command_in_dir: %s; output: \n%s\n--------------------------'%(returncode, output))
+
+    success = pynml.execute_command_in_dir_with_realtime_output(command, run_dir, prefix="Output [%s]: "%command, verbose=False) 
+
+    print('  ----  Success of execute_command_in_dir_with_realtime_output: %s'%success)
