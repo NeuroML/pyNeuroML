da0d31bfb0ac32d0f40212b3544a2e0f7aaeb9c2
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-06-11 11:01:14 +0100

    fix: drop unused import
    This was somehow causing errors where instead of looking for `List` in
    `typing`, it was looking for `List` in `numpy.typing`---pylab must
    import it somewhere.
    
    For reference, here's the complete traceback:
    
    ```
    Traceback (most recent call last):
      File "/opt/hostedtoolcache/Python/3.11.12/x64/bin/pynml-modchananalysis", line 5, in <module>
        from pyneuroml.neuron.analysis.HHanalyse import main
      File "/opt/hostedtoolcache/Python/3.11.12/x64/lib/python3.11/site-packages/pyneuroml/neuron/analysis/HHanalyse.py", line 141, in <module>
        def get_states(txt: str) -> typing.List[str]:
                                    ^^^^^^^^^^^
      File "/opt/hostedtoolcache/Python/3.11.12/x64/lib/python3.11/site-packages/numpy/typing/__init__.py", line 189, in __getattr__
        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
    AttributeError: module 'numpy.typing' has no attribute 'List'
    ```

--- pyneuroml/neuron/analysis/HHanalyse.py
+++ pyneuroml/neuron/analysis/HHanalyse.py
@@ -14,7 +14,6 @@ from math import log
 
 import matplotlib.pyplot as pylab
 import neuron
-from pylab import *
 
 from pyneuroml.utils import get_state_color
 from pyneuroml.utils.cli import build_namespace
